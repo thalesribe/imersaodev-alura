114ce18e3c07b39dcab7308e67d1737a0b02fd64
thalesribe <thaleseduardo8@gmail.com>
2025-03-31 14:41:26 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+valorPokeCoin = prompt ("Digite um valor em PokeCoin")
+
+PokeCoinUSD = 0.01;
+PokeCoinBR = 0.05;
+PokeCoinEUR = 0.009;
+alert("O valor convertido em dolar e " + (valorPokeCoin * PokeCoinUSD).toLocaleString("en-US", { style: "currency", currency: "USD" }))
+alert("O valor convertido em real e " + (valorPokeCoin * PokeCoinBR).toLocaleString("pt-BR", { style: "currency", currency: "BRL" }))
+alert("O valor convertido em euro e " + (valorPokeCoin * PokeCoinEUR).toLocaleString("eu-EU", { style: "currency", currency: "EUR" }))
